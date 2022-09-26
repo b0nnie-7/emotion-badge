056167e5edf90c7e7b99109719dda56bc8078a89
b0nnie-7 <113034423+b0nnie-7@users.noreply.github.com>
2022-09-26 09:23:00 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Confused)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.Angry)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
